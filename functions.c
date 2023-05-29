8327993c8a98765666eebaff890a36263a558ee7
Williams <88431152+williamsgqdev@users.noreply.github.com>
2023-05-29 14:25:27 +0100

    basics of functions in C

---
+++ functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void birthday()
+{
+    printf("\n Happy Birthday to Yoy");
+    printf("\n Happy Birthday to Yoy");
+    printf("\n Happy Birthday , Happy Birthday , Happy Birthday to Yoy");
+}
+int main()
+{
+    birthday();
+    
+    birthday();
+    birthday();
+
+    return 0;
+}
