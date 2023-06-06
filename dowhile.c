4f872b81967c2d04db31bea3c9bf4b3603bc5d5c
Williams <88431152+williamsgqdev@users.noreply.github.com>
2023-06-06 15:32:21 +0100

    post

---
+++ dowhile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 0;
+    int sum = 0;
+
+    do
+    {
+        printf("Enter a number above zero");
+
+        scanf("%d", &number);
+
+        if (number > 0)
+        {
+            sum += number;
+        }
+    } while (number > 0);
+    return 0;
+}
